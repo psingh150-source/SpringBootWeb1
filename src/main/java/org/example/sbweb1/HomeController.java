7a0d5b1b1b0514c6446e3c5b57f4625ee9e929e6
Prakriti Singh <prsing@griddynamics.com>
2026-03-01 16:28:13 +0530

    removed httpservletreq

--- src/main/java/org/example/sbweb1/HomeController.java
+++ src/main/java/org/example/sbweb1/HomeController.java
@@ -1,6 +1,6 @@
 package org.example.sbweb1;
 
-import jakarta.servlet.http.HttpServletRequest;
+
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +13,9 @@ public class HomeController {
         return "index.jsp";
     }
     @RequestMapping("addNumbers")
-    public String add(HttpServletRequest req, HttpSession session) {
-        int num1 = Integer.parseInt(req.getParameter("num1"));
-        int num2 = Integer.parseInt(req.getParameter("num2"));
-        int result = num1 + num2;
+    public String add(int num1, int num2, HttpSession session) {
+
+        int result = num1 + num2 + 1;
         session.setAttribute("result", result);
 
 
