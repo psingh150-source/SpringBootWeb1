1eb1694690ce7212b343508a000fa84f50c100b6
Prakriti Singh <prsing@griddynamics.com>
2026-03-02 15:54:36 +0530

    JSP to Thymeleaf

--- src/main/java/org/example/sbweb1/HomeController.java
+++ src/main/java/org/example/sbweb1/HomeController.java
@@ -1,26 +1,31 @@
 package org.example.sbweb1;
 
 
-import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+    @ModelAttribute("course")
+    public String courseName()
+    {
+        return "Java";
+    }
+
     @RequestMapping("/")
-    public String home(){
-        return "index.jsp";
+    public String home()
+    {
+        return "index.html";
     }
     @RequestMapping("addNumbers")
-    public String add(int num1, int num2, HttpSession session) {
+    public String add(int num1, int num2, Model model)
+    {
 
-        int result = num1 + num2 + 1;
-        session.setAttribute("result", result);
-
-
-        return "result.jsp";
+        int result = num1 + num2;
+        model.addAttribute("result", result);
+        return "result.html";
     }
-
-
 }
--- src/main/webapp/result.jsp
+++
@@ -1,7 +0,0 @@
-<%@ page language="java" %>
-
-<html>
-   <body>
-      <h2> RESULT IS : ${result} </h2>
-   </body>
-</html>
